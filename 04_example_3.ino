8babd2994f09c12cc409361593321f378d32d9ea
Dong-Hun Kang <96365479+skykang25@users.noreply.github.com>
2023-09-14 10:19:28 +0900

    upload 04_example_3

---
+++ 04_example_3.ino
@@ -0,0 +1,18 @@
+#define PIN_LED 13
+unsigned int count, toggle;
+void setup(){
+  pinMode(PIN_LED,OUTPUT);
+  Serial.begin(115200);
+  while (!Serial){
+    ;
+  }
+  Serial.println("Hello World!");
+  count = toggle = 0;
+  digitalWrite(PIN_LED, toggle);
+}
+void loop(){
+  Serial.println(++count);
+  toggle = (toggle ? 0 : 1);
+  digitalWrite(PIN_LED, toggle);
+  delay(1000);
+}
