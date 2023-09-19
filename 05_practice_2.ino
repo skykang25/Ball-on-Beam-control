3368fbd2a370fb123b0ac45fcd80c88883370a46
Dong-Hun Kang <96365479+skykang25@users.noreply.github.com>
2023-09-19 10:17:04 +0900

    Add 05_practice_2.ino

---
+++ 05_practice_2.ino
@@ -0,0 +1,18 @@
+#define PIN_LED 7
+void setup() {
+  pinMode(PIN_LED,OUTPUT);
+}
+
+void loop() {
+    digitalWrite(PIN_LED, 0);
+    delay(1000);
+    for (int i=1; i<=5; i++){
+        digitalWrite(PIN_LED, 1);
+        delay(100);
+        digitalWrite(PIN_LED, 0);
+        delay(100);
+    }
+    while (1){
+      digitalWrite(PIN_LED, 1);
+    }
+}
